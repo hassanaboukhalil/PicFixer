6380ba6ae81b4ea0de7c1e1ea0d83e7778412e5b
hassan <hassanak509@gmail.com>
2025-04-13 14:11:53 +0300

    created an js file to include the data needed for the UI for the upload images options

---
+++ client/src/components/pages-components/SelectSource/sourceOptionsData.js
@@ -0,0 +1,24 @@
+import { ImageIcon, CameraIcon } from 'lucide-react';
+
+export const sourceOptions = [
+    {
+        label: 'Pictures Library',
+        icon: ImageIcon,
+        onClick: () => {
+            window.electronAPI
+                .openPicturesLibrary()
+                .then((result) => console.log('Selected image:', result))
+                .catch((err) => console.error('Error selecting image:', err));
+        },
+    },
+    {
+        label: 'Camera',
+        icon: CameraIcon,
+        onClick: () => {
+            window.electronAPI
+                .openCamera()
+                .then((result) => console.log('Image captured from webcam:', result))
+                .catch((err) => console.error('Camera capture failed:', err));
+        },
+    },
+];
