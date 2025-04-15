b63d0fbba457483d6577a0c4c73defdea9bb11db
hassan <hassanak509@gmail.com>
2025-04-15 10:45:20 +0300

    updated the axios.js file

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -3,9 +3,8 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const axiosBaseUrl = axios.create({
-    // baseURL: 'http://localhost:8000/api/v1',
     // baseURL: 'http://52.47.74.242/snippet-server/api/v1',
-    baseURL: 'http://127.0.0.1:8000/api/v1',
+    baseURL: 'http://127.0.0.1:8000/api',
     headers: {
         'Content-Type': 'application/json',
         // ...(token && { 'Authorization': `Bearer ${token}` })
