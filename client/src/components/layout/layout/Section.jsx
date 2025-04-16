a032c3f086df6f5323b298a998dff1b3e916ae63
hassan <hassanak509@gmail.com>
2025-04-16 17:16:28 +0300

    deleted unused files

--- client/src/components/layout/layout/Section.jsx
+++
@@ -1,6 +0,0 @@
-const Section = ({ children, className, bgColor = 'bg-tertiary', marginTop = 'mt-8' }) => {
-    return (
-        <section className={`container ${className} ${bgColor} ${marginTop}`}>{children}</section>
-    );
-};
-export default Section;
