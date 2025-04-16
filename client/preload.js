eaadc0e0ee916f6661587fedf277932526d4b166
hassan <hassanak509@gmail.com>
2025-04-16 17:11:15 +0300

    added the saveImage funtion in the preload.js file

--- client/preload.js
+++ client/preload.js
@@ -4,4 +4,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
     openPicturesLibrary: () => ipcRenderer.invoke('open-pictures-library'),
     openCamera: () => ipcRenderer.invoke('open-camera'),
+    saveImage: (base64) => ipcRenderer.invoke('save-image', base64),
 });
