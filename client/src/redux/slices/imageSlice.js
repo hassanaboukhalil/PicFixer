616212e0000b2c7d4838e7ad83ddd52a427d116b
hassan <hassanak509@gmail.com>
2025-04-24 21:35:52 +0300

    updated the ImageSlice to update the state in a better way

--- client/src/redux/slices/imageSlice.js
+++ client/src/redux/slices/imageSlice.js
@@ -1,4 +1,3 @@
-// redux/imageSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
 const imageSlice = createSlice({
@@ -13,31 +12,64 @@ const imageSlice = createSlice({
     },
     reducers: {
         setImageBase64: (state, action) => {
-            state.base64 = action.payload;
+            // state.base64 = action.payload;
+            return {
+                ...state,
+                base64: action.payload,
+            };
         },
         rotateLeft: (state) => {
-            state.rotation -= 90;
+            // state.rotation -= 90;
+            return {
+                ...state,
+                rotation: state.rotation - 90,
+            };
         },
         rotateRight: (state) => {
-            state.rotation += 90;
+            // state.rotation += 90;
+            return {
+                ...state,
+                rotation: state.rotation + 90,
+            };
         },
         toggleBW: (state) => {
             state.isBW = !state.isBW;
         },
+
         setWatermark: (state, action) => {
-            state.watermarkText = action.payload;
+            // state.watermarkText = action.payload;
+            return {
+                ...state,
+                watermarkText: action.payload || 'PicFixer',
+            };
         },
         saveImage: (state, action) => {
-            state.savedPath = action.payload;
+            // state.savedPath = action.payload;
+            return {
+                ...state,
+                savedPath: action.payload,
+            };
         },
         enableCropping: (state) => {
-            state.isCropping = true;
+            // state.isCropping = true;
+            return {
+                ...state,
+                isCropping: true,
+            };
         },
         disableCropping: (state) => {
-            state.isCropping = false;
+            // state.isCropping = false;
+            return {
+                ...state,
+                isCropping: false,
+            };
         },
         resetRotation: (state) => {
-            state.rotation = 0;
+            // state.rotation = 0;
+            return {
+                ...state,
+                rotation: 0,
+            };
         },
 
         clearImage: (state) => {
@@ -57,11 +89,11 @@ export const {
     rotateRight,
     toggleBW,
     setWatermark,
-    clearImage,
     saveImage,
     enableCropping,
     disableCropping,
     resetRotation,
+    clearImage,
 } = imageSlice.actions;
 
 export default imageSlice;
