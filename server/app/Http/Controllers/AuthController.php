c5200f3376f4e8a0c056f514e9f6de5e1aca724e
hassan <hassanak509@gmail.com>
2025-04-25 03:26:16 +0300

    added the signup function inside the AuthController.php & updated the file to make it more readable

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -3,55 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Events\UserLoggedIn;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Traits\ResponseTrait;
+use App\Services\AuthService;
 
 class AuthController extends Controller
 {
-    function login(Request $request)
+    function login(LoginRequest $request)
     {
         try {
-            $email = $request->email;
-            $password = $request->password;
-            $ip = $request->ip;
-            $lat = $request->latitude;
-            $lon = $request->longitude;
-
-            if (!$email || !$password) {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'Email and password are required'
-                ], 500);
-            }
-
-            $user = User::where('email', $email)->first();
-            // $user_password = User::where($user_email);
-
-            if (!$user || !Hash::check($password, $user->password)) {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'Wrong email or password'
-                ], 401);
-            }
-
-            UserLoggedIn::dispatch($user, $ip, $lat, $lon);
-
-            return response()->json([
-                'success' => true,
-                // 'user' => $user,
-                'message' => 'User logged in successfully',
-                'user' => [
-                    'id' => $user->id,
-                    'email' => $user->email,
-                    'name' => $user->name
-                ],
-            ], 200);
+            $authService = new AuthService();
+            return $authService->login($request->only([
+                'email',
+                'password',
+                'ip',
+                'latitude',
+                'longitude'
+            ]));
         } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => $e->getMessage(),
-            ], 500);
+            return $this->errorResponse($e->getMessage(), 500);
+        }
+    }
+
+    function signup(SignupRequest $request)
+    {
+        try {
+            $authService = new AuthService();
+            return $authService->signup($request->only([
+                'name',
+                'email',
+                'password',
+                'ip',
+                'latitude',
+                'longitude'
+            ]));
+        } catch (\Exception $e) {
+            return $this->errorResponse($e->getMessage(), 500);
         }
     }
 }
