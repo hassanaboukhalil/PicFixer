5ad380c322004f8a2ebcfaf7a87348fd344b3436
hassan <hassanak509@gmail.com>
2025-04-25 03:27:23 +0300

    added in the frontend the route for the signup page in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home';
 import Signup from './pages/Signup';
-// import Login from "./pages/login";
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
@@ -15,8 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
-                {/* <Route path="/signup" element={<Signup />} /> */}
-                {/* <Route path="/home" element={<Home />} /> */}
+                <Route path="/signup" element={<Signup />} />
                 <Route path="/select-source" element={<SelectSource />} />
                 <Route path="/image-editor" element={<ImageEditor />} />
                 <Route path="/*" element={<h1>404 - Page Not Found</h1>} />
