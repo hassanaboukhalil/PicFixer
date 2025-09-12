8b1d5b8f5dd2594d1de97c62e27e8ce4031d5513
hassan <hassanak509@gmail.com>
2025-09-12 08:23:44 +0300

    added UserObserver to handle user events

---
+++ server/app/Observers/UserObserver.php
@@ -0,0 +1,54 @@
+<?php
+
+// namespace App\Observers;
+
+// use App\Models\LoginActivity;
+// use App\Models\user;
+
+// class UserObserver
+// {
+//     /**
+//      * Handle the user "created" event.
+//      */
+//     public function created(user $user): void
+//     {
+//         LoginActivity::create([
+//             'user_id' => $user->id,
+//             'ip_address' => '194.233.123.423',
+//             'latitude' => '35.23223',
+//             'longitude' => '34.32324'
+//         ]);
+//     }
+
+//     /**
+//      * Handle the user "updated" event.
+//      */
+//     public function updated(user $user): void
+//     {
+//         //
+//     }
+
+//     /**
+//      * Handle the user "deleted" event.
+//      */
+//     public function deleted(user $user): void
+//     {
+//         //
+//     }
+
+//     /**
+//      * Handle the user "restored" event.
+//      */
+//     public function restored(user $user): void
+//     {
+//         //
+//     }
+
+//     /**
+//      * Handle the user "force deleted" event.
+//      */
+//     public function forceDeleted(user $user): void
+//     {
+//         //
+//     }
+// }
