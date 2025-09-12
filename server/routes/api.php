7607a047afd106bdd7f9fae3d9dd4aa273b32a4d
hassan <hassanak509@gmail.com>
2025-09-12 08:33:40 +0300

    add API route for adding users with admin middleware

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/signup', [AuthController::class, 'signup']);
+
+
+Route::group(['prefix' => "v1", "middleware" => 'auth:api'], function () {
+    Route::group(['middleware' => 'isAdmin'], function () {
+        Route::post('/add-user', [AuthController::class, 'login']);
+    });
+});
