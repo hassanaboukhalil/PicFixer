83b389b1e0ea4e6441043e0df6217ae895eb0611
hassan <hassanak509@gmail.com>
2025-09-22 15:09:10 +0300

    frontend: wrap routes in WatermarkDialogStateProvider for watermark dialog state management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,22 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import SelectSource from './pages/SelectSource';
 import ImageEditor from './pages/ImageEditor';
+import WatermarkDialogStateProvider from './state/context/DialogWatermarkContext';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/select-source" element={<SelectSource />} />
-                <Route path="/image-editor" element={<ImageEditor />} />
-                <Route path="/*" element={<h1>404 - Page Not Found</h1>} />
-            </Routes>
-        </BrowserRouter>
+        <WatermarkDialogStateProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/select-source" element={<SelectSource />} />
+                    <Route path="/image-editor" element={<ImageEditor />} />
+                    <Route path="/*" element={<h1>404 - Page Not Found</h1>} />
+                </Routes>
+            </BrowserRouter>
+        </WatermarkDialogStateProvider>
     );
 }
 
