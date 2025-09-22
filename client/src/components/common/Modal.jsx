d5ac008aff4c6b3cb616b8b573b99895a575b056
hassan <hassanak509@gmail.com>
2025-09-22 15:40:24 +0300

    frontend: enhance Modal component styling and structure

--- client/src/components/common/Modal.jsx
+++ client/src/components/common/Modal.jsx
@@ -1,16 +1,43 @@
+import React from 'react';
 import { X } from 'lucide-react';
 
 const Modal = ({ children, handleCloseModal, title, className }) => {
     return (
-        <div className="modal-overlay w-full h-full flex justify-center items-center px-8 py-8">
-            <div className={`modal bg-white rounded-lg flex flex-column gap-4 ${className}`}>
-                <div
-                    className="bg-white flex justify-end cursor-pointer"
-                    onClick={handleCloseModal}
-                >
-                    <X />
+        <div
+            className="modal-overlay w-full h-full flex justify-center items-center px-8 py-8"
+            style={{
+                width: '100vw',
+                height: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: '2rem',
+            }}
+        >
+            <div
+                className={`modal rounded-lg flex flex-column gap-4 ${className}`}
+                style={{
+                    backgroundColor: '#1e1e1e',
+                    padding: '16px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '1rem',
+                    borderRadius: '8px',
+                    border: '1px solid #808080',
+                }}
+            >
+                <div className="flex justify-between">
+                    <h4 className="h3">{title}</h4>
+                    <div
+                        className="cursor-pointer"
+                        style={{
+                            cursor: 'pointer',
+                        }}
+                        onClick={handleCloseModal}
+                    >
+                        <X />
+                    </div>
                 </div>
-                <h4 className="h3 mb-4 relative flex flex-center">{title}</h4>
                 {children}
             </div>
         </div>
