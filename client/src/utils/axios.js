5e01a3ac843b21576db26df235df16a6b0c93213
hassan <hassanak509@gmail.com>
2025-09-25 17:47:58 +0300

    frontend: update axios base URL to use 'localhost' instead of '127.0.0.1'

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,7 +4,8 @@ const token = localStorage.getItem('token');
 
 const axiosBaseUrl = axios.create({
     // baseURL: 'http://52.47.74.242/snippet-server/api/v1',
-    baseURL: 'http://127.0.0.1:8000/api',
+    // baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
         // ...(token && { 'Authorization': `Bearer ${token}` })
